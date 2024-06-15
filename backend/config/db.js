e9c248971f95cacb1d3b0ffba31ea37ccde56f56
Amoda <amodahegde@gmail.com>
2024-06-15 17:47:43 +0530

    Changes 1

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+import { connect } from 'mongoose';
+import { get } from 'config';
+const db = get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
